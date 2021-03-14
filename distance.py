692f2feb546940d79f078e99170ac7043dd5a79d
bunnyrabbit022 <bunnyrabbit022@users.noreply.github.com>
2021-03-13 22:32:40 -0500

    not getting kar sued

--- distance.py
+++ distance.py
@@ -1,8 +1,9 @@
 import requests
 import json
 import googlemaps
+from config import apikey
 
 def distance(driverAddress, passengerAddress):
-    gmaps = googlemaps.Client(key = 'AIzaSyAvRTa3F8YCA3zIfLcLZyrG9FyA5m3WUe8')
+    gmaps = googlemaps.Client(key = apikey)
     distancestring = gmaps.distance_matrix(driverAddress, passengerAddress)['rows'][0]['elements'][0]['distance']['text']
     return float(distancestring[:len(distancestring)-3])
